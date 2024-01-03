9a54ce34b82892260d5be12a2b58c48ae2089c17
Jibril Mohamud <jibrilmohamod@gmail.com>
2024-01-03 12:24:37 +0300

    Add TheNav component and update styles in app.vue

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <NavigationMenuRoot class="h-20 border border-red-600 fixed w-full">
+    <NavigationMenuList> </NavigationMenuList>
+  </NavigationMenuRoot>
 </template>
 
 <script lang="ts" setup></script>
